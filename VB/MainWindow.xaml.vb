c94930db52861aa9ee488e46518a28e77a5ca8cb
DevExpressExampleBot <fakeEmail>
2018-06-21 05:01:06 +0300

    Source auto update [skip ci]

---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Windows
+Imports DevExpress.XtraReports.UI
+
+Namespace CustomizeReportDesignerToolbar
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub CloseDocumentButton_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            If reportDesigner.ActiveDocument IsNot Nothing Then
+                reportDesigner.ActiveDocument.Close()
+            End If
+        End Sub
+
+        Private Sub AboutButton_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            MessageBox.Show("This example demonstrates how to customize the Report Designer's toolbar.", "About")
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            reportDesigner.OpenDocument(New XtraReport())
+        End Sub
+    End Class
+End Namespace
